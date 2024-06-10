90b3c40076cc42b9ff379c2e918f58fb74a236d8
Mohammad Hamza Ibrahim <mi469@live.mdx.ac.uk>
2024-06-10 20:40:09 +0100

    Received forecast data and presented on frontend

--- src/Components/ForecastWeather.jsx
+++ src/Components/ForecastWeather.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 
-const ForecastWeather = () => {
+const ForecastWeather = ({ forecastData, city }) => {
+
+    if (Object.keys(forecastData).length === 0) {
+        return <p>Loading current forecast data...</p>;
+    }
+    console.log(forecastData.list)
+    const days = forecastData.list.filter((item, index) => index % 8 === 0).slice(0, 4);
+    // console.log();
+
+    const forecast = days.map((day, index) => {
+
+        const dayName = new Date(day.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long' })
+        const date = new Date(day.dt * 1000).toLocaleDateString('en-GB')
+
+        return (
+            <div key={index} className="col-lg-3 col-md-6 col-sm-6 col-6" style={{ flex: 1 }}>
+                <h3>Forecast for {dayName}</h3>
+                <div><img src={`/assets/weather-icons/${day.weather[0].icon}.svg`} alt="Weather Icon" /></div>
+                <div>Date: {date}</div>
+                <div>Weather: {day.weather[0].description}</div>
+                <div>Temperature: {Math.round(day.main.temp - 273.15)}°C</div>
+            </div>
+        )
+    })
+
+    // { new Date(item.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long' }) }
+
     return (
-        <div>ForecastWeather</div>
-    )
+        <>
+            <div className="row">
+                {forecast}
+            </div>
+
+        </>
+    );
+    // };
 }
 
 export default ForecastWeather
